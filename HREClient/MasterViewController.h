9c04cedbb021863f804abb9776de495c055bb133
yashigani <tai.fukui@gmail.com>
2014-02-15 13:11:12 +0900

    Initial Commit

---
+++ HREClient/MasterViewController.h
@@ -0,0 +1,13 @@
+//
+//  MasterViewController.h
+//  HREClient
+//
+//  Created by taiki on 2/15/14.
+//  Copyright (c) 2014 taiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+@end
---
+++ HREClient/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HREClient
+//
+//  Created by taiki on 2/15/14.
+//  Copyright (c) 2014 taiki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
